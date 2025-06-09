39c0c1a4d0881aaf93e3fb5a0a41a79c5d0f0631
Hal <haln_01@proton.me>
2025-06-09 12:57:15 -0700

    formatted code

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -1,18 +1,18 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import { createRoot } from "react-dom/client";
-import Home from "./App";
-import { Auth, AuthGithub } from "./pages/auth";
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { createRoot } from 'react-dom/client';
+import Home from './App';
+import { Auth, AuthGithub } from './pages/auth';
 
 let router = createBrowserRouter([
-	{
-		path: "/",
-		Component: Home,
-		children: [],
-	},
-	{ path: "/auth", Component: Auth },
-	{ path: "/auth/github", Component: AuthGithub },
+  {
+    path: '/',
+    Component: Home,
+    children: [],
+  },
+  { path: '/auth', Component: Auth },
+  { path: '/auth/github', Component: AuthGithub },
 ]);
 
-createRoot(document.getElementById("root")).render(
-	<RouterProvider router={router} />,
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />,
 );
--- webui/src/pages/auth.tsx
+++ webui/src/pages/auth.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useSearchParams, redirect } from "react-router-dom";
+import { useEffect } from 'react';
+import { useSearchParams, redirect } from 'react-router-dom';
 // path /auth
 export function Auth() {
   useAuth();
@@ -10,7 +10,7 @@ export function Auth() {
 function useAuth() {
   const [param] = useSearchParams();
   useEffect(() => {
-    const sessionId = param.get("session_id");
+    const sessionId = param.get('session_id');
     (async () => {
       const r = await fetch(
         // TODO: change the API url, pull from env
@@ -29,13 +29,13 @@ export function AuthGithub() {
 function useAuthGithub() {
   const [param] = useSearchParams();
   useEffect(() => {
-    const sessionId = param.get("session_id");
-    const oauthCode = param.get("code");
-    const oauthState = param.get("state");
-    const deviceType = param.get("device_type");
+    const sessionId = param.get('session_id');
+    const oauthCode = param.get('code');
+    const oauthState = param.get('state');
+    const deviceType = param.get('device_type');
 
     if (!sessionId || !oauthCode || !oauthState || !deviceType) {
-      throw new Error("Invalid params.");
+      throw new Error('Invalid params.');
     }
 
     (async () => {
@@ -45,10 +45,7 @@ function useAuthGithub() {
         state: oauthState,
         device_type: deviceType,
       });
-      const r = await fetch(
-        // TODO: change the API url, pull from env
-        `http://localhost:8000/api/auth/token?${p.toString()}`,
-      );
+      const r = await fetch(`/api/auth/token?${p.toString()}`);
       console.log(r.status);
     })();
   }, []);
