8bdd205f09a33b0fd2833fe14eb0c5146b21cddf
Hal <haln_01@proton.me>
2025-06-09 12:49:11 -0700

    login button for home page

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useNavigate } from "react-router";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const h = useHome();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<button onClick={h.signInRedirect}>Sign In with GitHub</button>
+		</>
+	);
 }
 
-export default App
+export default App;
+
+function useHome() {
+	const nav = useNavigate();
+
+	async function signInRedirect() {
+		const r = await fetch(`/api/auth?device_type=web`);
+		const url = new URL(await r.text());
+		nav(`${url.pathname}${url.search}`);
+	}
+
+	return { signInRedirect };
+}
