11fa6269904a4d4388bd5eeb41992d0e2773d35e
Hal <haln_01@proton.me>
2025-06-10 10:56:48 -0700

    renew session + cache repo fetching request

--- src/server/user_handlers.py
+++ src/server/user_handlers.py
@@ -1,17 +1,19 @@
 from dataclasses import asdict
-import json
 from .config import *
 from shared.types import Cookie
 from typing import Optional
 from fastapi.routing import APIRouter
 from fastapi import Depends, HTTPException, Cookie
 from fastapi.responses import JSONResponse
-from fastapi import Response
 
 
 def auth_middleware(evault_access_token: Optional[str] = Cookie(None)) -> str:
     if evault_access_token == None:
         raise HTTPException(status_code=403, detail="Access token not found/expired.")
+
+    # extend the session
+    redis.renew_user_session(evault_access_token, EVAULT_SESSION_TOKEN_TTL)
+
     return evault_access_token
 
 
@@ -34,12 +36,19 @@ def get_user_information(evault_access_token: str = Depends(auth_middleware)):
 def get_user_repositories(evault_access_token: str = Depends(auth_middleware)):
     d = redis.get_user_session(evault_access_token)
     assert d != None
+
     (_, token) = d
     repos = httpclient.fetch_user_repositories(token.token_type, token.access_token)
-    return Response(
+
+    body = [asdict(r) for r in repos]
+    headers = {
+        "Cache-Control": "max-age=120",
+    }
+
+    return JSONResponse(
         status_code=200,
-        media_type="application/json; charset=utf-8",
-        content=json.dumps([asdict(repo) for repo in repos]),
+        content=body,
+        headers=headers,
     )
 
 
