ed8778abcb0b06c45d0826fbfca5b63ab71ef005
Hal <haln_01@proton.me>
2025-06-10 17:23:40 -0700

    useQuery tanstack not working :(

--- webui/src/routes/dashboard/repository/$repoID.tsx
+++ webui/src/routes/dashboard/repository/$repoID.tsx
@@ -1,5 +1,14 @@
 import { createFileRoute, useLoaderData } from "@tanstack/react-router";
 import { useEffect } from "react";
+import {
+  QueryClient,
+  useMutation,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { useForm } from "@tanstack/react-form";
+
+// Create a client
+const queryClient = new QueryClient();
 
 export const Route = createFileRoute("/dashboard/repository/$repoID")({
   component: RouteComponent,
@@ -30,11 +39,41 @@ function RouteComponent() {
   const repo = useLoaderData({ from: "/dashboard/repository/$repoID" });
   useEffect(() => console.log(repo), [repo]);
 
+  const { form } = useNewRepository();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       {repo === null ? (
         <>
-          <button>Create new vault</button>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              form.handleSubmit();
+            }}
+          >
+            <form.Field
+              name="password"
+              children={(field) => (
+                <input
+                  type="password"
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+              )}
+            />
+            <form.Subscribe
+              selector={(state) => [state.canSubmit, state.isSubmitting]}
+              children={([canSubmit, isSubmitting]) => (
+                <button type="submit" disabled={!canSubmit}>
+                  {isSubmitting ? "..." : "Submit"}
+                </button>
+              )}
+            />
+          </form>
         </>
       ) : typeof repo === "number" ? (
         <>{repo}</>
@@ -43,7 +82,37 @@ function RouteComponent() {
           <section>Hello {` /dashboard/repository/${repo!.id}`}!</section>
         </>
       )}
-      <div></div>
-    </>
+    </QueryClientProvider>
   );
+}
+
+type NewFormProps = {
+  repo_id: string;
+  password: string;
+};
+
+function useNewRepository() {
+  const mut = useMutation({
+    mutationKey: ["newRepoForm"],
+    mutationFn: async (data: NewFormProps) => {
+      return new Promise((resolve) => resolve("ok")).then((_) => {
+        console.log(data);
+      });
+    },
+  });
+
+  const form = useForm({
+    defaultValues: {
+      password: "",
+    },
+    onSubmit: async ({ value }) => {
+      mut.mutate({
+        repo_id: "123",
+        password: value.password,
+      });
+      console.log(value);
+    },
+  });
+
+  return { form };
 }
