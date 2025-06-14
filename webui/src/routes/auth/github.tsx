dd957e309e225007c6bd555531dbb7f8a0129def
Hal <haln_01@proton.me>
2025-06-14 16:23:09 -0700

    added spinner and loading texts for `/auth/github`

--- webui/src/routes/auth/github.tsx
+++ webui/src/routes/auth/github.tsx
@@ -1,10 +1,15 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 import {
   createFileRoute,
   useSearch,
   useNavigate,
+  Link as RouterLink,
 } from "@tanstack/react-router";
 import { useState, useEffect } from "react";
 import { z } from "zod/v4";
+import { Link, CircularProgress, Stack, Typography } from "@mui/material";
+import { AnimatePresence, motion } from "framer-motion";
 
 const paramParser = z.object({
   session_id: z.string(),
@@ -23,49 +28,93 @@ export const Route = createFileRoute("/auth/github")({
 
 function RouteComponent() {
   const f = useAuthGithub();
-  return f.loading ? (
-    <>Authenticating...</>
-  ) : f.error !== null ? (
-    <> {f.error}</>
+
+  const { index, loadingTexts } = useLoadingText();
+
+  return f.status === "pending" ? (
+    <Stack
+      gap={4}
+      mt="30vh"
+      justifyItems="center"
+      alignItems="center"
+      mx="auto"
+    >
+      <Typography variant="h4">Authenticating with GitHub</Typography>
+
+      <Stack justifyItems="center" alignItems="center" gap={2}>
+        <CircularProgress />
+        <AnimatePresence mode="wait">
+          {loadingTexts.map((text, idx) => {
+            return index === idx ? (
+              <motion.div
+                key={`text-key-${idx}`}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <Typography variant="body1">{text}</Typography>
+              </motion.div>
+            ) : null;
+          })}
+        </AnimatePresence>
+      </Stack>
+    </Stack>
+  ) : f.status === "error" ? (
+    <Stack mt="30vh" mx="auto" width="max-content" alignItems="center" gap={1}>
+      <Typography variant="h4">Authentication Failed</Typography>
+      <Link component={RouterLink} to="/">
+        Go back home.
+      </Link>
+    </Stack>
   ) : (
-    <>Authenticated, you can now close this window.</>
+    <></>
   );
 }
 
+function useLoadingText() {
+  const loadingTexts = [
+    "Authenticating with secure provider...",
+    "Verifying your identity...",
+    "Syncing your security credentials...",
+    "Confirming sharing privileges...",
+    "Checking vault sharing authorization...",
+  ];
+
+  const lineCtr = loadingTexts.length;
+
+  const [index, setIndex] = useState(Math.floor(Math.random() * lineCtr));
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const nextIndex = Math.floor(Math.random() * lineCtr);
+      setIndex(nextIndex);
+    }, 2500);
+    return () => clearInterval(interval);
+  }, [lineCtr]);
+
+  return { index, loadingTexts };
+}
+
 function useAuthGithub() {
-  const param = useSearch({ from: "/auth/github" });
+  const params = useSearch({ from: "/auth/github" });
   const nav = useNavigate();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+
+  const {
+    isPending: loading,
+    error,
+    status,
+  } = useQuery({
+    queryKey: ["githubauth"],
+    queryFn: () => axios.get("/api/auth/token", { params }),
+    retry: 0,
+  });
 
   useEffect(() => {
-    const { data: searchParam, error: searchParamError } =
-      paramParser.safeParse(param);
-
-    if (searchParamError) {
-      setError("Invalid authentication parameters.");
-      console.error(searchParamError.message);
-      setLoading(false);
-      return;
+    if (status === "success") {
+      nav({ to: "/dashboard" });
+    } else if (status === "error") {
+      console.error(error);
     }
+  }, [nav, status, error]);
 
-    const p = new URLSearchParams(searchParam);
-
-    (async () => {
-      try {
-        await fetch(`/api/auth/token?${p.toString()}`);
-
-        if (searchParam.device_type === "web") {
-          nav({ to: "/dashboard" });
-        }
-      } catch (e) {
-        setError("Something went wrong.");
-        console.error(e);
-      } finally {
-        setLoading(false);
-      }
-    })();
-  }, [nav, param]);
-
-  return { loading, error };
+  return { loading, status };
 }
