b9f2e02ac004026a9e3b5f5eb7d198bdb9438d3f
Hal <haln_01@proton.me>
2025-06-15 22:59:39 -0700

    github auth UI with dynamic loading texts

--- webui/src/routes/auth/github.tsx
+++ webui/src/routes/auth/github.tsx
@@ -1,23 +1,88 @@
+import { useEffect, useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
-import { z } from "zod/v4";
-import { paramParser } from "../../utils/zod/gitHubParams";
+import {
+  paramParser,
+  type GitHubOAuthSearchParamResult,
+} from "../../utils/zod/gitHubParams";
 import { useAuthGithub } from "../../hooks/auth";
+import { CircularProgress, Link, Stack, Typography } from "@mui/material";
+import { AnimatePresence, motion } from "motion/react";
+import { Link as RouterLink } from "@tanstack/react-router";
 
-type SearchParam = z.infer<typeof paramParser>;
+const TEXT_CHANGE_INTERVAL = 3000; // milliseconds
+const loadingTexts = [
+  "Authenticating with secure provider...",
+  "Verifying your identity...",
+  "Syncing your security credentials...",
+  "Confirming sharing privileges...",
+  "Checking vault sharing authorization...",
+];
 
 export const Route = createFileRoute("/auth/github")({
-  validateSearch: (s: Record<string, unknown>): SearchParam =>
-    paramParser.parse(s),
+  validateSearch: (s: Record<string, unknown>): GitHubOAuthSearchParamResult =>
+    paramParser.safeParse(s),
   component: RouteComponent,
 });
 
 function RouteComponent() {
   const f = useAuthGithub();
-  return f.loading ? (
-    <>Authenticating...</>
-  ) : f.error !== null ? (
-    <> {f.error}</>
+  const { index } = useLoadingText(TEXT_CHANGE_INTERVAL);
+
+  return f.status === "pending" || true ? (
+    <Stack
+      gap={4}
+      mt="30vh"
+      justifyItems="center"
+      alignItems="center"
+      mx="auto"
+    >
+      <Typography variant="h4" color="textPrimary">
+        Authenticating with GitHub
+      </Typography>
+
+      <Stack justifyItems="center" alignItems="center" gap={2}>
+        <CircularProgress />
+        <AnimatePresence mode="wait">
+          {loadingTexts.map((text, idx) => {
+            return index === idx ? (
+              <motion.div
+                key={`text-key-${idx}`}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 10 }}
+              >
+                <Typography variant="body1" color="textSecondary">
+                  {text}
+                </Typography>
+              </motion.div>
+            ) : null;
+          })}
+        </AnimatePresence>
+      </Stack>
+    </Stack>
+  ) : f.status === "error" ? (
+    <Stack mt="30vh" mx="auto" width="max-content" alignItems="center" gap={1}>
+      <Typography variant="h4">Authentication Failed</Typography>
+      <Link component={RouterLink} to="/">
+        Go back home.
+      </Link>
+    </Stack>
   ) : (
-    <>Authenticated, you can now close this window.</>
+    <></>
   );
+}
+
+function useLoadingText(changeInterval: number) {
+  const lineCtr = loadingTexts.length;
+  const [index, setIndex] = useState(Math.floor(Math.random() * lineCtr));
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      const nextIndex = Math.floor(Math.random() * lineCtr);
+      setIndex(nextIndex);
+    }, changeInterval);
+    return () => clearInterval(intervalID);
+  }, [lineCtr]);
+
+  return { index };
 }
