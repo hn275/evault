756892eb661e263e1fe87a68ee079c5fe5052612
Hal <haln_01@proton.me>
2025-06-15 22:54:56 -0700

    added a global `queryClient`

--- webui/src/routes/__root.tsx
+++ webui/src/routes/__root.tsx
@@ -1,10 +1,12 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { NotificationsProvider } from "@toolpad/core/useNotifications";
+import { queryClient } from "../utils/queryClient";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <QueryClientProvider client={queryClient}>
       <NotificationsProvider
         slotProps={{
           snackbar: {
@@ -21,6 +23,6 @@ export const Route = createRootRoute({
         <Outlet />
         <TanStackRouterDevtools />
       </NotificationsProvider>
-    </>
+    </QueryClientProvider>
   ),
 });
