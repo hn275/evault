528c0d5f6ec88b6ee5f92fd44af55cbb60120ce2
Hal <haln_01@proton.me>
2025-06-15 22:58:11 -0700

    typing for GitHubOAuth param parser

--- webui/src/services/auth.ts
+++ webui/src/services/auth.ts
@@ -1,4 +1,5 @@
 import type { User } from "../types/User";
+import type { GitHubOAuthSearchParams } from "../utils/zod/gitHubParams";
 import { fetchWithRedirect } from "./common";
 
 // Fetch the user's information from the API
@@ -8,6 +9,9 @@ export async function getUser(): Promise<User> {
 }
 
 // Exchange the GitHub authentication
-export async function getGitHubAuth(p: URLSearchParams) {
+export function getGitHubAuth(
+  params: GitHubOAuthSearchParams,
+): Promise<Response> {
+  const p = new URLSearchParams(params);
   return fetch(`/api/auth/token?${p.toString()}`);
 }
