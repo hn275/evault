2306c325821f38a7d122ccfc217d0240ce0667a3
Hal <haln_01@proton.me>
2025-06-15 22:54:56 -0700

    added a global `queryClient`

--- webui/src/routes/dashboard.tsx
+++ webui/src/routes/dashboard.tsx
@@ -1,4 +1,3 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { Outlet } from "@tanstack/react-router";
 
@@ -9,10 +8,5 @@ export const Route = createFileRoute("/dashboard")({
 function LayoutComponent() {
   // TODO: user needs to be logged in to access the dashboard if user is not logged in, redirect to the login page
 
-  const queryClient = new QueryClient();
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Outlet />
-    </QueryClientProvider>
-  );
+  return <Outlet />;
 }
