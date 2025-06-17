0dc7ec162a3bd97a3ce438ab5ab6ec8b54914050
Hal <haln_01@proton.me>
2025-06-17 13:56:42 -0700

    fixed tests for UserSession <-> redis mapping functions

--- tests/server/test_user_session_marshalling.py
+++ tests/server/test_user_session_marshalling.py
@@ -1,16 +1,16 @@
 from src.pkg.types import GitHubUser, GithubAuthToken, UserSession
 from copy import deepcopy
 
-flat_map = {
+test_data = {
     "device_type": "web",
     "user.id": 1,
     "user.name": "Test User 1",
     "user.avatar_url": "",
     "user.login": "testuser1",
     "user.type": "User",
-    "token.access_token": "usertoken1",
-    "token.token_type": "bearer",
-    "token.scope": "",
+    "gh_token.access_token": "usertoken1",
+    "gh_token.token_type": "bearer",
+    "gh_token.scope": "",
 }
 
 test_user = GitHubUser(
@@ -27,13 +27,19 @@ test_token = GithubAuthToken(
     scope="",
 )
 
-test_session = UserSession("web", user=test_user, token=test_token)
+test_session = UserSession("web", user=test_user, gh_token=test_token)
 
 
 def test_user_session_marshalling():
     s = deepcopy(test_session)
-    assert s.make_flat_map() == flat_map
+    assert s.make_flat_map() == test_data
 
-    f = deepcopy(flat_map)
+    f = deepcopy(test_data)
     s1 = UserSession.from_flat_map(f)
     assert s1 == s
+    assert s1.csrf_token == None
+
+    f["csrf_token"] = "foobarbaz"
+    s1 = UserSession.from_flat_map(f)
+    assert s1 != s
+    assert s1.csrf_token == f["csrf_token"]
