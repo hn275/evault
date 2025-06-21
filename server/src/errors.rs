f6729cb2059688603948c5c1ceab151ecab52070
Hal <haln_01@proton.me>
2025-06-21 01:54:39 -0700

    authenticating with github TODO: fetch user info + email from github and store in db, set reponse cookie `evault_access_token`

--- server/src/errors.rs
+++ server/src/errors.rs
@@ -1,36 +1,25 @@
 use axum::{Json, http::StatusCode, response::IntoResponse};
-use serde::Serialize;
+use serde_json::json;
 use tracing::error;
-
-pub struct ApiError {
-    pub detail: String,
-    pub code: StatusCode,
-}
 
 pub enum AppError {
     Internal(anyhow::Error),
-    Response(ApiError),
+    Response(StatusCode, String),
 }
 
 pub type Result<T> = std::result::Result<T, AppError>;
 
-#[derive(Serialize)]
-struct ErrorMessage {
-    detail: String,
-}
 impl IntoResponse for AppError {
     fn into_response(self) -> axum::response::Response {
         match self {
             AppError::Internal(err) => {
                 error!("Something went wrong: {}", err);
-                let msg = ErrorMessage {
-                    detail: String::from("Something went wrong."),
-                };
-                (StatusCode::INTERNAL_SERVER_ERROR, Json(msg)).into_response()
+                let body = json!({ "detail": err.to_string() });
+                (StatusCode::INTERNAL_SERVER_ERROR, Json(body)).into_response()
             }
-            AppError::Response(err) => {
-                let msg = ErrorMessage { detail: err.detail };
-                (err.code, Json(msg)).into_response()
+            AppError::Response(code, err) => {
+                let body = json!({ "detail": err });
+                (code, Json(body)).into_response()
             }
         }
     }
--- server/src/github.rs
+++ server/src/github.rs
@@ -1,8 +1,12 @@
+use std::time::Duration;
+
 use anyhow::Context;
-use secrecy::SecretString;
+use reqwest::header::{ACCEPT, HeaderValue};
+use secrecy::{ExposeSecret, SecretString};
+use serde::Deserialize;
 use url::Url;
 
-use crate::errors::Result;
+use crate::errors::{AppError, Result};
 use crate::utils::env::{env_or_default, env_or_panic};
 
 const GITHUB_OAUTH_SCOPE: &'static str = "repo read:user";
@@ -12,10 +16,36 @@ pub struct GitHubAPI {
     oauth_client_id: String,
     oauth_client_secret: SecretString,
     oauth_redirect_uri: String,
+
+    http: reqwest::Client,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GitHubAuthToken {
+    access_token: String,
+    token_type: String,
+    scope: String,
 }
 
 impl GitHubAPI {
     pub fn new() -> Self {
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert(
+            "X-GitHub-Api-Version",
+            HeaderValue::from_static("2022-11-28"),
+        );
+        headers.insert(
+            ACCEPT,
+            HeaderValue::from_static("application/vnd.github+json"),
+        );
+
+        let http = reqwest::Client::builder()
+            .default_headers(headers)
+            .cookie_store(true)
+            .timeout(Duration::from_secs(10))
+            .build()
+            .unwrap();
+
         Self {
             oauth_client_id: env_or_panic("GITHUB_OAUTH_CLIENT_ID"),
             oauth_client_secret: env_or_panic("GITHUB_OAUTH_CLIENT_SECRET").into(),
@@ -23,13 +53,53 @@ impl GitHubAPI {
                 "GITHUB_OAUTH_REDIRECT_URI",
                 "http://localhost:5173/auth/github",
             ),
+
+            http,
         }
+    }
+
+    pub async fn fetch_user_auth_token(&self, auth_code: &str) -> Result<GitHubAuthToken> {
+        let url = self.make_github_oauth_url(auth_code)?;
+        let response = self
+            .http
+            .post(url)
+            .send()
+            .await
+            .context("Failed to build request")?;
+
+        if response.status() != reqwest::StatusCode::OK {
+            Err(AppError::Response(
+                response.status().into(),
+                "Failed to fetch user credentials from GitHub.".to_string(),
+            ))?;
+        }
+
+        Ok(response
+            .json::<GitHubAuthToken>()
+            .await
+            .context("Failed to parse GitHub access token response")?)
+    }
+
+    fn make_github_oauth_url(&self, code: &str) -> Result<String> {
+        let mut oauth_url =
+            Url::parse("https://github.com").context("Failed to parse GitHub URL")?;
+        oauth_url
+            .path_segments_mut()
+            .map_err(|_| anyhow::anyhow!("Invalid GitHub URL"))?
+            .extend(&["login", "oauth", "access_token"]);
+        oauth_url
+            .query_pairs_mut()
+            .append_pair("client_id", &self.oauth_client_id)
+            .append_pair("client_secret", &self.oauth_client_secret.expose_secret())
+            .append_pair("code", code);
+
+        Ok(oauth_url.to_string())
     }
 
     pub fn make_login_url(
         &self,
-        state: &str,
         session_id: &str,
+        state: &str,
         device_type: &str,
     ) -> Result<String> {
         let mut redirect_url =
