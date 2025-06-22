b8094a522c71c99aa5b1035a2a426fafb0793da0
Brian Pham <nguyenphamswork@gmail.com>
2025-06-22 11:14:41 -0700

    removing universal nav bar and updating docs

--- webui/src/routes/__root.tsx
+++ webui/src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { NotificationsProvider } from "@toolpad/core/useNotifications";
 import { queryClient } from "../utils/queryClient";
@@ -14,12 +14,6 @@ export const Route = createRootRoute({
           },
         }}
       >
-        <div className="p-2 flex gap-2">
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>{" "}
-        </div>
-        <hr />
         <Outlet />
         <TanStackRouterDevtools />
       </NotificationsProvider>
