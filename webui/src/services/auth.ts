eb4c4f53aff2bfae64f5fc0071ed0717d100e7dd
Hal <haln_01@proton.me>
2025-06-28 12:12:02 -0700

    fixed ci for webui

--- webui/src/services/auth.ts
+++ webui/src/services/auth.ts
@@ -1,5 +1,6 @@
 import type { User } from "../types/User";
 import { httpClient } from "../utils/axios";
+import { type AxiosPromise } from "axios";
 import type { GitHubOAuthSearchParams } from "../utils/zod/gitHubParams";
 
 // Fetch the user's information from the API
@@ -11,7 +12,7 @@ export async function getUser(): Promise<User> {
 // Exchange the GitHub authentication
 export function getGitHubAuth(
   params: GitHubOAuthSearchParams,
-): Promise<Response> {
+): AxiosPromise<string> {
   const p = new URLSearchParams(params);
   return httpClient.get(`/auth/token?${p.toString()}`);
 }
