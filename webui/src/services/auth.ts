8802ee66668e154bf2cea53871166e7eb39a7eef
Brian Pham <nguyenphamswork@gmail.com>
2025-06-30 22:08:23 -0700

    adding Title to all pages

--- webui/src/services/auth.ts
+++ webui/src/services/auth.ts
@@ -6,8 +6,7 @@ import type { GitHubOAuthSearchParams } from "../utils/zod/gitHubParams";
 export async function getUser(): Promise<User> {
   const r = await httpClient.get("/dashboard/user", {
     transformResponse: (data) => {
-      // TODO: adding zod type validation
-      return data;
+      return JSON.parse(data);
     },
   });
   return r.data;
